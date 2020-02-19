174bc62298b188e0c005112a18b4246231f43f6c
Koen Bisschop <koen.bisschop@hotmail.be>
2020-02-19 08:07:40 +0100

    Start Throwing

---
+++ StartWithInheritance/StartWithInheritance/Cirkel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace StartWithInheritance
+{
+    class Cirkel
+    {
+        const UInt32 MAX_SIZE = 200;
+
+        private static Int32 _aantal = 0;
+        public static Int32 Aantal
+        {
+            get
+            {
+                return _aantal;
+            }
+        }
+        private int _nummer;
+        public int Nummer
+        {
+            get
+            {
+                return _nummer;
+            }
+            private set
+            {
+                _nummer = value;
+            }
+        }
+
+        UInt32 _straal;
+        public UInt32 Straal
+        {
+            get
+            {
+                return _straal;
+            }
+            set
+            {
+                if (value > MAX_SIZE)
+                {
+                    //mag niet!
+                    throw new Exception("Straal is groter dan " + MAX_SIZE);
+                }
+                else
+                {
+                    _straal = value;
+                }
+            }
+        }
+        public Cirkel (UInt32 straal )
+        {
+            _aantal++;
+            Nummer = _aantal;
+
+            Straal = straal;
+        }
+
+        public void Paint(Graphics g)
+        {
+            Pen pen = new Pen(new SolidBrush(Color.Black));
+            g.FillEllipse(new SolidBrush(Color.Yellow), 
+                Straal, Straal, Straal, Straal);
+            g.DrawEllipse(pen, Straal, Straal, Straal, Straal);
+
+        }
+
+        public override String ToString()
+        {
+            return $"cirkel {Nummer} met straal {Straal}";
+        }
+    }
+}
---
+++ StartWithInheritance/StartWithInheritance/frmVormen.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace StartWithInheritance
+{
+    partial class frmVormen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmVormen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "frmVormen";
+            this.Text = "Vormen";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
